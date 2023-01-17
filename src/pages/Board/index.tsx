0c162cf91981de7b453528c2ee2621f3a6141130
WOOJINBAEK <bwj59@naver.com>
2023-01-17 18:54:24 +0900

    FE-36 [FEAT] 보드 타이틀 클릭으로 변경 구현

--- src/pages/Board/index.tsx
+++ src/pages/Board/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Board from '@/components/Board/Board';
 import * as S from '@/pages/Board/indexStyle';
 import { useParams } from 'react-router-dom';
@@ -8,10 +9,42 @@ function BoardPage() {
   let { boardId } = useParams(); // 추후에 board를 구분하는 변수로 사용!
   const [boards, setBoards] = useRecoilState(boardsAtom);
   let [board] = boards.filter((board: any) => board.boardId === boardId);
+  const [boardTitle, setBoardTitle] = useState(board.boardTitle);
+
+  const handleChange = (e: any) => {
+    setBoardTitle((prev) => e.target.value);
+  };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === 'Enter' || e.key === 'Escape') {
+      removeFocus();
+      e.preventDefault();
+      handleSaveData();
+      let newBoards = boards.map((board) => (board.boardId === boardId ? { ...board, boardTitle: boardTitle } : board));
+      setBoards((prev) => newBoards);
+    }
+  };
+
+  const removeFocus = () => {
+    (document.activeElement as HTMLElement).blur();
+  };
+  const handleSaveData = () => {
+    let newBoards = boards.map((board) => (board.boardId === boardId ? { ...board, boardTitle: boardTitle } : board));
+    setBoards((prev) => newBoards);
+  };
 
   return (
     <S.BoardWrapper>
-      <S.BoardTitle>{board.boardTitle}</S.BoardTitle>
+      <S.BoardTitle
+        spellCheck="false"
+        boardTitle={boardTitle}
+        value={boardTitle}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        onBlur={() => {
+          handleSaveData();
+        }}
+      ></S.BoardTitle>
       <Board boards={boards} setBoards={setBoards} boardId={boardId} />
     </S.BoardWrapper>
   );
--- src/pages/Board/indexStyle.tsx
+++ src/pages/Board/indexStyle.tsx
@@ -5,7 +5,22 @@ export const BoardWrapper = styled.div`
   background-color: #a8a8a8;
 `;
 
-export const BoardTitle = styled.div`
-  font-size: 1.5rem;
-  padding: 30px;
+export const BoardTitle = styled.textarea<{ boardTitle: any }>`
+  cursor: pointer;
+  width: ${(props) => `${props.boardTitle.length * 23}px`};
+  min-width: 200px;
+  font-size: 1.3rem;
+  height: 40px;
+  margin: 10px 0px 0px 0px;
+  padding: 4px 8px;
+  border: none;
+  resize: none;
+  border-radius: 3px;
+  background: transparent;
+  &:focus {
+    cursor: text;
+    background: white;
+    box-shadow: inset 0 0 0 2px #ee8d0d;
+    outline: 0;
+  }
 `;
