e0259214fc4be706bf26e7e016592587e49f4ece
“gayoung” <tp06100@naver.com>
2023-01-19 15:39:54 +0900

    FE-85 사이드바 인라인 스타일 적용 수정

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,34 +1,8 @@
 import * as S from './SidebarStyle';
 import { ReactComponent as Logo } from '@/assets/images/logo.svg';
-import { ReactComponent as Dashboard } from '@/assets/images/dashboard.svg';
-import { ReactComponent as Setting } from '@/assets/images/setting.svg';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { AiOutlineHome, AiOutlineLeft, AiOutlineSetting, AiOutlineCalendar, AiOutlineTable } from 'react-icons/ai';
 import { useLocation } from 'react-router-dom';
-import styled from 'styled-components';
-
-const ModalBackdrop = styled.div`
-  width: 100%;
-  height: 100%;
-  position: fixed;
-  z-index: 999;
-  display: flex;
-  flex-flow: row wrep;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.5);
-`;
-
-const ModalView = styled.div`
-  text-align: center;
-  text-decoration: none;
-  padding: 30px 90px;
-  background-color: white;
-  width: 20vw;
-  height: 60vh;
-  border-radius: 30px;
-  color: #4000c7;
-`;
 
 const linksArray = [
   {
@@ -47,7 +21,7 @@ const secondaryLinksArray = [
   {
     label: 'Table',
     icon: <AiOutlineTable />,
-    to: '',
+    to: '/workspace',
   },
   {
     label: 'Calender',
@@ -75,18 +49,12 @@ const board = [
 ];
 
 function Sidebar() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(true);
   const { pathname } = useLocation();
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  const openModalHandler = () => {
-    setIsOpen(!isOpen);
-  };
 
   const changeHandler = (e: MediaQueryListEvent) => {
     {
-      mediaQueryList.matches ? setSidebarOpen(true) : setSidebarOpen(false);
+      mediaQueryList.matches ? setSidebarOpen(false) : setSidebarOpen(true);
     }
   };
 
@@ -94,58 +62,45 @@ function Sidebar() {
   mediaQueryList.addEventListener('change', changeHandler);
 
   return (
-    <>
-      <S.SidebarWrapper isOpen={sidebarOpen}>
-        <S.SidebarOpenButton isOpen={sidebarOpen} onClick={() => setSidebarOpen((p) => !p)}>
-          <AiOutlineLeft />
-        </S.SidebarOpenButton>
-        <S.LogoLink to={'/'}>
-          <Logo width="45px" height="45px" />
-          <S.LogoText isOpen={sidebarOpen}>Timyloom</S.LogoText>
-        </S.LogoLink>
-        <S.Divider />
-        {linksArray.map(({ icon, label, to }) => (
+    <S.SidebarWrapper isOpen={sidebarOpen}>
+      <S.SidebarOpenButton isOpen={sidebarOpen} onClick={() => setSidebarOpen((p) => !p)}>
+        <AiOutlineLeft />
+      </S.SidebarOpenButton>
+      <S.LogoLink to={'/'}>
+        <Logo width="45px" height="45px" />
+        <S.LogoText isOpen={sidebarOpen}>Timyloom</S.LogoText>
+      </S.LogoLink>
+      <S.Divider />
+      {linksArray.map(({ icon, label, to }) => (
+        <S.SLinkWrapper key={label} isActive={pathname === to}>
+          <S.SLink to={to} isOpen={sidebarOpen}>
+            <S.LinkIcon>{icon}</S.LinkIcon>
+            {sidebarOpen && <S.LinkLabel>{label}</S.LinkLabel>}
+          </S.SLink>
+        </S.SLinkWrapper>
+      ))}
+      <S.SidebarSubtitle isOpen={sidebarOpen}>Workspace Views</S.SidebarSubtitle>
+      <S.Divider />
+      <S.ViewContainer isOpen={sidebarOpen}>
+        {secondaryLinksArray.map(({ icon, label, to }) => (
           <S.SLinkWrapper key={label} isActive={pathname === to}>
-            <S.SLink to={to} style={sidebarOpen ? { width: `fit-content` } : {}}>
+            <S.SLink to="/" isOpen={sidebarOpen} column={true}>
               <S.LinkIcon>{icon}</S.LinkIcon>
-              {!sidebarOpen && <S.LinkLabel>{label}</S.LinkLabel>}
+              {sidebarOpen && <S.LinkLabel column={true}>{label}</S.LinkLabel>}
             </S.SLink>
           </S.SLinkWrapper>
         ))}
-        <S.SidebarSubtitle isOpen={sidebarOpen}>Workspace Views</S.SidebarSubtitle>
-        <S.Divider />
-        <S.ViewContainer isOpen={sidebarOpen}>
-          {secondaryLinksArray.map(({ icon, label, to }) => (
-            <S.SLinkWrapper key={label} isActive={pathname === to}>
-              <S.SLink
-                to="/"
-                style={sidebarOpen ? { width: `fit-content` } : { width: '120px', flexDirection: 'column' }}
-              >
-                <S.LinkIcon>{icon}</S.LinkIcon>
-                {!sidebarOpen && <S.LinkLabel style={{ paddingRight: '0px' }}>{label}</S.LinkLabel>}
-              </S.SLink>
-            </S.SLinkWrapper>
-          ))}
-        </S.ViewContainer>
-        <S.SidebarSubtitle isOpen={sidebarOpen}>Your Boards</S.SidebarSubtitle>
-        <S.Divider />
-        <S.BoardContainer isOpen={sidebarOpen}>
-          {board.map((item, index) => (
-            <S.BoardWrapper key={index} boardDesign={item.color} onClick={openModalHandler}>
-              {item.title}
-            </S.BoardWrapper>
-          ))}
-        </S.BoardContainer>
-      </S.SidebarWrapper>
-      {isOpen ? (
-        <ModalBackdrop onClick={openModalHandler}>
-          <ModalView>
-            <div onClick={openModalHandler}>&times;</div>
-            <div>Hello World</div>
-          </ModalView>
-        </ModalBackdrop>
-      ) : null}
-    </>
+      </S.ViewContainer>
+      <S.SidebarSubtitle isOpen={sidebarOpen}>Your Boards</S.SidebarSubtitle>
+      <S.Divider />
+      <S.BoardContainer isOpen={sidebarOpen}>
+        {board.map((item, index) => (
+          <S.BoardWrapper key={index} boardDesign={item.color}>
+            {item.title}
+          </S.BoardWrapper>
+        ))}
+      </S.BoardContainer>
+    </S.SidebarWrapper>
   );
 }
 
