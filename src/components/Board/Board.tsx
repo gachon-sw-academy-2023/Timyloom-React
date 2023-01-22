b086cdf4ac9231ff2ec09e0c606820e30638fe9d
WOOJINBAEK <bwj59@naver.com>
2023-01-23 08:52:03 +0900

    FE-46 [REFACTOR] 카드<->카드 드래그앤드랍 코드 리팩토링

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -27,38 +27,23 @@ function Board({ boards, setBoards, boardId }: any) {
   };
 
   const reorderCardPosition = (source: any, destination: any, cardId: any, boardId: any) => {
-    // let tempBoards = JSON.parse(JSON.stringify(boards));
-    // let tempBoard = JSON.parse(JSON.stringify(board));
-    // let tempSourceList = tempBoard.lists.filter((list: any) => list.listId === source.droppableId)[0];
-    // let tempDestinationList = tempBoard.lists.filter((list: any) => list.listId === destination.droppableId)[0];
-    // let [reorderCard] = tempSourceList.cards.splice(source.index, 1);
-    // tempDestinationList.cards.splice(destination.index, 0, reorderCard);
-    // tempBoard.lists.map((list: any) => (list.listId === tempSourceList.listId ? tempSourceList : list));
-    // tempBoard.lists.map((list: any) => (list.listId === tempDestinationList.listId ? tempDestinationList : list));
-    // let newBoards = tempBoards.map((board: any) => (board.boardId === tempBoard.boardId ? tempBoard : board));
-    // setBoards((prev: any) => newBoards);
     let tempBoard = JSON.parse(JSON.stringify(boards)).filter((board: any) => board.boardId === boardId)[0];
     let tempSourceCards = tempBoard.lists.filter((list: any) => list.listId === source.droppableId)[0].cards;
     let tempDestinationCards = tempBoard.lists.filter((list: any) => list.listId === destination.droppableId)[0].cards;
     let [reorderCard] = tempSourceCards.splice(source.index, 1);
     tempDestinationCards.splice(destination.index, 0, reorderCard);
-    console.log(tempSourceCards, tempDestinationCards);
-    console.log(tempBoard);
-
-    //여기가 마지막 작업 상태 ===  현재 카드들을 옮겨야 하는 리스트로 옮긴 상태
-    //해야하는 일은 바뀐 리스트들을 적용해야 하는 단계입니다.
+    let newBoard = tempBoard.lists.map((list: any) =>
+      source.droppableId === list.listId ? { ...list, cards: tempSourceCards } : list,
+    );
+    newBoard = tempBoard.lists.map((list: any) =>
+      destination.droppableId === list.listId ? { ...list, cards: tempDestinationCards } : list,
+    );
+    setBoards((prev: any) =>
+      boards.map((board: any) => (boardId === board.boardId ? { ...board, lists: newBoard } : board)),
+    );
   };
 
   const reorderListPosition = (sourceIndex: any, destinationIndex: any, boardId: any) => {
-    // 리펙토링 전 코드
-    // let tempBoards = JSON.parse(JSON.stringify(boards));
-    // let tempBoard = JSON.parse(JSON.stringify(board));
-    // let [reorderList] = tempBoard.lists.splice(sourceIndex, 1);
-    // tempBoard.lists.splice(destinationIndex, 0, reorderList);
-    // let newBoards = tempBoards.map((board: any) => (board.boardId === tempBoard.boardId ? tempBoard : board));
-    // setBoards((prev: any) => newBoards);
-
-    //리펙토링 후 코드
     let tempLists = JSON.parse(JSON.stringify(boards)).filter((board: any) => board.boardId === boardId)[0].lists;
     let [reorderList] = tempLists.splice(sourceIndex, 1);
     tempLists.splice(destinationIndex, 0, reorderList);
