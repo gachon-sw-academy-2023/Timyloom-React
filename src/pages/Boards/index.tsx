8af1bfa16a6879216aa1765ed695dd6be956817c
WOOJIN BAEK <45286570+bwj0509@users.noreply.github.com>
2023-01-31 00:05:24 +0900

    FE-87 [FEAT] 워크스페이스 로딩 Skeleton 구현

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -3,6 +3,8 @@ import * as S from '@/pages/Boards/indexStyle';
 import { useRecoilState } from 'recoil';
 import { boardsAtom } from '@/recoil/boardsAtom';
 import { defaultData } from '@/components/Board/BoardData';
+import { useState } from 'react';
+import Skeleton from '@mui/material/Skeleton';
 
 const handleAddBoard = (boards: any, setBoards: any) => {
   let newData = defaultData();
@@ -11,8 +13,31 @@ const handleAddBoard = (boards: any, setBoards: any) => {
 };
 
 function Boards({ sidebarOpen }: any) {
+  const [loading, setLoading] = useState(true);
   const [boards, setBoards] = useRecoilState(boardsAtom);
   let personalBoards = boards.filter((board) => board.owner === localStorage.getItem('id'));
+
+  setTimeout(() => {
+    setLoading((prev) => false);
+  }, 1000);
+
+  if (loading)
+    return (
+      <S.PageWrapper isopen={sidebarOpen}>
+        <WorkspaceHeader />
+        <S.BoardContainer>
+          <S.SkeletonWrapper>
+            <Skeleton variant="rounded" animation="wave" width={255} height={170} />
+          </S.SkeletonWrapper>
+          <S.SkeletonWrapper>
+            <Skeleton variant="rounded" animation="wave" width={255} height={170} />
+          </S.SkeletonWrapper>
+          <S.SkeletonWrapper>
+            <Skeleton variant="rounded" animation="wave" width={255} height={170} />
+          </S.SkeletonWrapper>
+        </S.BoardContainer>
+      </S.PageWrapper>
+    );
 
   return (
     <S.PageWrapper isopen={sidebarOpen}>
