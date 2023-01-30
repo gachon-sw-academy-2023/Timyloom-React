1175400ec31f4819130dcbae7738d9f4a4b1a3fc
WOOJINBAEK <bwj59@naver.com>
2023-01-30 17:25:51 +0900

    [CHORE] ListTitle 변경 함수 분리

--- src/components/Board/List.tsx
+++ src/components/Board/List.tsx
@@ -6,7 +6,6 @@ import ListTitle from './ListTitle';
 function List({ listId, listData, index, boardId }: any) {
   return (
     <Draggable draggableId={listId} index={index}>
-      {/* 기존 index는 listData.position를 사용하는데 map함수의 index를 사용하면 어떨까..?  */}
       {(provided) => (
         <S.ListWrapper ref={provided.innerRef} {...provided.draggableProps}>
           <S.ListContent>
