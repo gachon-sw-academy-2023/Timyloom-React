ec634bd6e3938af9e2f07e0270740f516f83ea86
Han <bluesushi264@gmail.com>
2023-02-02 11:07:35 +0900

    FE-97 [FEAT] 보드 삭제 페이지 디자인 수정

--- src/components/RemoveCard/RemoveCard.tsx
+++ src/components/RemoveCard/RemoveCard.tsx
@@ -36,6 +36,7 @@ function RemoveCard() {
 
   return (
     <S.CardWrapper>
+      <S.GearSvg />
       <S.CardTitle size="1.5rem">보드를 삭제하였습니다.</S.CardTitle>
       <S.CardTitle size="1.3rem">삭제된 보드는 30일간 보관되며 이후에 영구삭제 됩니다.</S.CardTitle>
       <Button themes="sign" onClick={handlePage}>
--- src/components/RemoveCard/RemoveCardStyle.tsx
+++ src/components/RemoveCard/RemoveCardStyle.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { ReactComponent as Gear } from '@/assets/images/gear.svg';
 
 export const CardWrapper = styled.div`
   padding: 30px;
@@ -10,7 +11,24 @@ export const CardWrapper = styled.div`
   border-radius: 15px;
 `;
 
+export const GearSvg = styled(Gear)`
+  width: 100px;
+  height: 100px;
+  margin-top: 15vh;
+  animation: rotation 2000ms infinite linear;
+
+  @keyframes rotation {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(359deg);
+    }
+  }
+`;
+
 export const CardTitle = styled.div<{ size: string }>`
+  width: 100%;
   font-size: ${(props) => props.size};
   margin: 10px;
 `;
