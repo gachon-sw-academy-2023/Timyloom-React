39002a03ac33447780e9dc0111bd276d38384f3b
WOOJINBAEK <bwj59@naver.com>
2023-02-07 16:06:40 +0900

    [CHORE] 변수명 변경: mentIndex -> welcomeMessageIndex

--- src/components/Board/Welcome.tsx
+++ src/components/Board/Welcome.tsx
@@ -2,11 +2,11 @@ import * as S from '@/components/Board/WelcomeStyle';
 import { TypeAnimation } from 'react-type-animation';
 
 interface WelcomeProps {
-  mentIndex: number;
+  welcomeMessageIndex: number;
   brightness: number;
 }
 
-function Welcome({ mentIndex, brightness }: WelcomeProps) {
+function Welcome({ welcomeMessageIndex, brightness }: WelcomeProps) {
   const sequence = [
     [`😀 Welcome ${localStorage.getItem('id')}.`, 2000, '👆 Make your first List.', 2000],
     ['👆 Make your Card!', 2000, '👆 Make everything with Timmyloom.', 2000],
@@ -14,7 +14,7 @@ function Welcome({ mentIndex, brightness }: WelcomeProps) {
 
   return (
     <S.WelcomeWrapper brightness={brightness}>
-      <TypeAnimation sequence={sequence[mentIndex]} wrapper="div" speed={75} repeat={Infinity} />
+      <TypeAnimation sequence={sequence[welcomeMessageIndex]} wrapper="div" speed={75} repeat={Infinity} />
     </S.WelcomeWrapper>
   );
 }
